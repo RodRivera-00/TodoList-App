5c11794e109ffc15aa3992d39058d414b9521ec3
yeonin <m.rivera.rodandrew@gmail.com>
2022-09-19 22:57:33 +0800

    Added saving token to json-server on login

--- test-next/pages/api/login/index.ts
+++ test-next/pages/api/login/index.ts
@@ -1,6 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
-import { LoginResponse, UserLogin } from "../../../types/user";
+import { LoginResponse, User, UserLogin } from "../../../types/user";
 import jwt from "jsonwebtoken";
 export default async function Login(
 	req: NextApiRequest,
@@ -21,15 +21,33 @@ export default async function Login(
 		);
 		const result = await checkLogin.json();
 		if (result.length === 1) {
+			const user: User = result[0];
 			//Set JWT token
-			const token = jwt.sign(result[0], process.env.JWT_SECRET || "", {
+			const token = jwt.sign(user, process.env.JWT_SECRET || "", {
 				expiresIn: "6h",
 			});
-			res.status(200).json({
-				error: false,
-				message: "Login successful.",
-				token: token,
-			});
+			//Save JWT Token
+			try {
+				//This can still be improved depending on the backend's error handling
+				await fetch("http://localhost:8081/users/" + user.id, {
+					method: "PATCH",
+					headers: {
+						Accept: "application/json",
+						"Content-Type": "application/json",
+					},
+					body: JSON.stringify({ token: token }),
+				});
+				res.status(200).json({
+					error: false,
+					message: "Login successful.",
+					token: token,
+				});
+			} catch (e: any) {
+				res.status(200).json({
+					error: true,
+					message: e.toString(),
+				});
+			}
 		} else {
 			res.status(200).json({
 				error: true,
